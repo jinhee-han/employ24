54922307dd622fed417dff25c823f3c07a46d1bf
Baek-seung-beom <seungbeom.baek@myvtw.co.kr>
2021-08-02 16:38:24 +0900

    유급휴업 CrossOrigin 추가

--- src/main/java/com/vtw/employ/openAPI/controller/BplcController.java
+++ src/main/java/com/vtw/employ/openAPI/controller/BplcController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping("/employ24/api")
 public class BplcController {
     @Autowired
--- src/main/java/com/vtw/employ/openAPI/controller/IpsController.java
+++ src/main/java/com/vtw/employ/openAPI/controller/IpsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping("/employ24/api")
 public class IpsController {
     @Autowired
